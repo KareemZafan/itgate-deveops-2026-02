4188e2c93b73a0caac8d456bfedcbf7df5dff25d
Kareem Mohamed <kareem.mohamed@adfinis.com>
2026-03-28 21:26:31 +0200

    Add two functions add, getMax

--- src/main.c
+++ src/main.c
@@ -1,12 +1,20 @@
-
-
 #include <stdio.h> 
+
+double add(double a, double b){
+  return a + b; 
+}
+
+double getMax(double a, double b){
+  return (a > b) ? a : b; 
+}
+
 
 int main(void){
   int a = 10 ; 
   int b = 20; 
 
-  printf("Sum = %d \n",a + b); 
+  printf("Sum = %d \n", add(a,b)); 
+  printf("Max = %d \n", getMax(a,b));
 
 return 0; 
 }
